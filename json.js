05b5301ce2c52534f1ee633a9842f672b9527cda
gaetanV <gaetan.vigneron@gmail.com>
2018-02-17 17:57:13 +0100

    Init Rust

---
+++ json.js
@@ -0,0 +1,4 @@
+[
+    {"partner_id":931,"endpoint":"https:\/\/t.tellapart.com\/twitterinitmatch","requires_guest_id":true},
+    {"partner_id":54,"endpoint":"https:\/\/t.tellapart.fr\/twitterinitmatch","requires_guest_id":false}
+]
